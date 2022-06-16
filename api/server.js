704dd47e47269bf12797473f311b3f6752bebdc6
marreropd <pablomarrerofernandez1@gmail.com>
2022-06-16 07:34:04 -0300

    axios call

--- api/server.js
+++ api/server.js
@@ -2,18 +2,18 @@ require("dotenv").config();
 
 const express = require("express");
 const routes = require("./routes");
-//const dbInitialSetup = require("./dbInitialSetup");
+
 const APP_PORT = process.env.APP_PORT || 3000;
 const app = express();
 
+const cors = require("cors");
+
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
+app.use(cors());
+
 app.use(express.json());
 
 routes(app);
-
-//dbInitialSetup(); // Crea tablas e inserta datos de prueba.
 
 app.listen(APP_PORT, () =>
   console.log(`\n[Express] Servidor corriendo en el puerto ${APP_PORT}!\n`),
