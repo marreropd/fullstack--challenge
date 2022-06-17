e3196f2ff8f3e6711a3092b8e66b1babff5c47d3
marreropd <pablomarrerofernandez1@gmail.com>
2022-06-16 21:34:51 -0300

    adding styles to ActionBotton (create new movement)

---
+++ front/src/components/ActionBotton/ActionBotton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./ActionBotton.css";
+function ActionBotton() {
+  return (
+    <div class="button">
+      <span class="icon">
+        <i class="bi bi-box-arrow-in-up-left"></i>
+      </span>
+      <span class="text">Añadir nuevo movimiento</span>
+    </div>
+  );
+}
+
+export default ActionBotton;
