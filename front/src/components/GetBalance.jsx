e18cd93851cbf3dad76285e0970c6f49afa0411a
marreropd <pablomarrerofernandez1@gmail.com>
2022-06-18 00:16:04 -0300

    this return the balance (income-degress) of the account

---
+++ front/src/components/GetBalance.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+function GetBalance() {
+  const store = useSelector((state) => state);
+  const movements = store.movements;
+
+  const totalAmounts = [];
+
+  for (let i = 0; i < movements.length; i++) {
+    if (movements[i].type === "Ingreso") {
+      totalAmounts.push(movements[i].amount);
+    } else {
+      totalAmounts.push(movements[i].amount * -1);
+    }
+  }
+  const result = totalAmounts.reduce((acc, amount) => acc + amount);
+
+  return <p>$ {result}</p>;
+}
+
+export default GetBalance;
