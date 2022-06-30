6e60efe358fdcdef4fedb4f90a08d974a6ce8b33
marreropd <pablomarrerofernandez1@gmail.com>
2022-06-30 19:17:23 -0300

    update

--- api/routes/publicRoutes.js
+++ api/routes/publicRoutes.js
@@ -2,14 +2,19 @@ const express = require("express");
 const publicRouter = express.Router();
 const userController = require("../controllers/userController");
 const movementController = require("../controllers/movementController");
-
+const { getToken } = require("../controllers/tokenController");
+const userRouter = express.Router();
+const checkJwt = require("express-jwt");
 // user
+publicRouter.post("/login", getToken);
+
 publicRouter.get("/users", userController.index);
 publicRouter.post("/users", userController.store);
 publicRouter.get("/users/:id", userController.show);
 publicRouter.delete("/users/:id", userController.destroy);
 
 //movements
+userRouter.use(checkJwt({ secret: process.env.ACCESS_TOKEN_SECRET, algorithms: ["HS256"] }));
 publicRouter.get("/movements", movementController.index);
 publicRouter.get("/movementsbyquery", movementController.indexByQuery);
 publicRouter.post("/movements", movementController.store);
