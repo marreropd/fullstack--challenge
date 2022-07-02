51a4ec4e60c6233ce3cdf70652a14c39449d3388
marreropd <pablomarrerofernandez1@gmail.com>
2022-07-02 19:40:01 -0300

    update

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -8,7 +8,7 @@ const { getToken, verifyToken } = require("../controllers/tokenController");
 Router.post("/login", getToken);
 
 Router.get("/users", verifyToken, userController.index);
-Router.post("/users", verifyToken, userController.store);
+Router.post("/users", userController.store);
 Router.get("/users/:id", verifyToken, userController.show);
 Router.delete("/users/:id", verifyToken, userController.destroy);
 
