3f271ec7c1e83c0b7d2b6ebce553b5df6dfdb7ca
marreropd <pablomarrerofernandez1@gmail.com>
2022-07-04 13:48:50 -0300

    update

--- api/controllers/tokenController.js
+++ api/controllers/tokenController.js
@@ -6,7 +6,7 @@ async function getToken(req, res) {
   const user = await User.findAll({ where: { email: req.body.email } });
 
   jwt.sign({ user: user[0] }, process.env.ACCESS_TOKEN_SECRET, (err, token) => {
-    res.json({ token: token });
+    res.send({ token: token });
   });
 }
 
