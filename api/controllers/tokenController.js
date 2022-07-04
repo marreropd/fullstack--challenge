e71d7594be0e849228c28c0c917a601fe857fca6
marreropd <pablomarrerofernandez1@gmail.com>
2022-07-04 12:19:16 -0300

    update

--- api/controllers/tokenController.js
+++ api/controllers/tokenController.js
@@ -5,10 +5,10 @@ const jwt = require("jsonwebtoken");
 async function getToken(req, res) {
   const user = await User.findAll({ where: { email: req.body.email } });
 
-  jwt.sign({ user: user[0] }, process.env.ACCESS_TOKEN_SECRET, (err, token) => {
-    res.json({ token: token });
+  const token = jwt.sign({ user: user[0] }, process.env.ACCESS_TOKEN_SECRET, (err, token) => {
+    return res.json({ token: token });
   });
-  res.json({ token: token });
+  res.json(token);
 }
 
 // middle
