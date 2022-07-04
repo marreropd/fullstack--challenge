ab40feea8e7f316d31661ea10117e4280db9e061
marreropd <pablomarrerofernandez1@gmail.com>
2022-07-04 15:46:12 -0300

    refactoring verify token

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -8,14 +8,14 @@ const { getToken, verifyToken } = require("../controllers/tokenController");
 Router.post("/login", getToken);
 
 Router.get("/users", verifyToken, userController.index);
-Router.post("/users", userController.store);
+Router.post("/users", verifyToken, userController.store);
 Router.get("/users/:id", verifyToken, userController.show);
 Router.delete("/users/:id", verifyToken, userController.destroy);
 
 //movements
-Router.get("/movements", movementController.index);
+Router.get("/movements", verifyToken, movementController.index);
 Router.get("/movementsbyquery", verifyToken, movementController.indexByQuery);
-Router.post("/movements", movementController.store);
+Router.post("/movements", verifyToken, movementController.store);
 Router.get("/movements/:id", verifyToken, movementController.show);
 Router.patch("/movements/:id", verifyToken, movementController.update);
 Router.delete("/movements/:id", verifyToken, movementController.destroy);
