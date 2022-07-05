4da0fa3892cc8c43ab686eb3d7ebe8c5e4309b9f
marreropd <pablomarrerofernandez1@gmail.com>
2022-07-05 18:43:02 -0300

    refactoring

--- front/src/components/Pages/Home/Home.jsx
+++ front/src/components/Pages/Home/Home.jsx
@@ -8,26 +8,6 @@ import LastMovements from "../../LastMovements/LastMovements";
 import Login from "../../Login";
 
 function Home() {
-  const store = useSelector((state) => state);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    getMovements();
-  }, []);
-
-  async function getMovements() {
-    try {
-      const response = await axios({
-        method: "GET",
-        url: `https://api-piggy.vercel.app/movements`,
-        headers: { Authorization: `Bearer ${store.user.token}` },
-      });
-      response.data && dispatch(movementActions.storeMovements(response.data));
-    } catch (error) {
-      console.log("Error: ", error);
-    }
-  }
-
   return (
     <div>
       {" "}
